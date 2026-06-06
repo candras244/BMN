85f123bf83494192c3e3fa12d4ce42d86e61b5f4
candras244 <candras244@gmail.com>
2026-06-06 21:11:57 +0700

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -17,62 +17,56 @@ document.getElementById("content");
 
 if(page === "dashboard"){
 
-    content.innerHTML = `
-        <h2>Dashboard</h2>
-        <p>Dashboard berhasil dimuat.</p>
-    `;
+    content.innerHTML =
+    "<h2>Dashboard</h2><p>Dashboard berhasil dimuat.</p>";
 
     return;
+
 }
 
 if(page === "gedung"){
 
-    content.innerHTML = `
-        <h2>Gedung</h2>
-        <p>Menu Gedung berhasil dimuat.</p>
-    `;
+    content.innerHTML =
+    "<h2>Gedung</h2><p>Menu Gedung berhasil dimuat.</p>";
 
     return;
+
 }
 
 if(page === "ruang"){
 
-    content.innerHTML = `
-        <h2>Ruang</h2>
-        <p>Menu Ruang berhasil dimuat.</p>
-    `;
+    content.innerHTML =
+    "<h2>Ruang</h2><p>Menu Ruang berhasil dimuat.</p>";
 
     return;
+
 }
 
 if(page === "aset"){
 
-    content.innerHTML = `
-        <h2>Master Aset</h2>
-        <p>Menu Master Aset berhasil dimuat.</p>
-    `;
+    content.innerHTML =
+    "<h2>Master Aset</h2><p>Menu Master Aset berhasil dimuat.</p>";
 
     return;
+
 }
 
 if(page === "dbr"){
 
-    content.innerHTML = `
-        <h2>DBR</h2>
-        <p>Menu DBR berhasil dimuat.</p>
-    `;
+    content.innerHTML =
+    "<h2>DBR</h2><p>Menu DBR berhasil dimuat.</p>";
 
     return;
+
 }
 
 if(page === "statistik"){
 
-    content.innerHTML = `
-        <h2>Statistik</h2>
-        <p>Menu Statistik berhasil dimuat.</p>
-    `;
+    content.innerHTML =
+    "<h2>Statistik</h2><p>Menu Statistik berhasil dimuat.</p>";
 
     return;
+
 }
 ```
 
