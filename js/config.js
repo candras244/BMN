7441604df5802120e20a381ef84f49230c5b3bc3
candras244 <candras244@gmail.com>
2026-06-06 21:40:11 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
 const API_URL =
-"https://script.google.com/macros/s/URL_APPS_SCRIPT_ANDA/exec";
+"https://script.google.com/macros/s/https://script.google.com/macros/s/AKfycbz5Teqx8w0VzpGfsq_Iomk5cOXKGU0-ASdi0K0JwckDv-kqBXjU-iEFBIQ05D9V9W6TjA/exec";
