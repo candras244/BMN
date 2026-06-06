3602a57b0c624383ef2ecc9e8640b2cf231bfed0
candras244 <candras244@gmail.com>
2026-06-06 20:52:57 +0700

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,5 +1,52 @@
 loadPage("dashboard");
+/* ==========================
+   GEDUNG
+========================== */
 
+if(page === "gedung"){
+
+    const gedung =
+    await getData("getGedung");
+
+    let html = `
+
+    <h2>Data Gedung</h2>
+
+    <br>
+
+    <div class="gedung-grid">
+
+    `;
+
+    gedung.forEach(item=>{
+
+        html += `
+
+        <div class="gedung-card">
+
+            <img
+            src="${item.fotoGedung}"
+            alt="${item.namaGedung}">
+
+            <div class="gedung-info">
+
+                <h3>
+                ${item.namaGedung}
+                </h3>
+
+            </div>
+
+        </div>
+
+        `;
+
+    });
+
+    html += `</div>`;
+
+    content.innerHTML = html;
+
+}
 function logout(){
 
     localStorage.removeItem(
