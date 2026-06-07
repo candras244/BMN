3d469c526079de6afadfb115fe1063534349847a
candras244 <candras244@gmail.com>
2026-06-07 17:43:57 +0700

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,7 @@
+const CONFIG = {
+    APP_NAME: "SIM-DBR",
+
+    API_URL: "https://script.google.com/macros/s/AKfycbwoT5GTdgNNn-xhaMEpqQnveqL8F7C4I-UZc1XgXVPqufLxuIbOovCS3fMvSqTlTFwy/exec",
+
+    VERSION: "1.0.0"
+};
